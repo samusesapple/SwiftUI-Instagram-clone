a59512c588ac5836d114e959f5d5f23f3ab42816
samusesapple <gwansammy@gmail.com>
2023-06-14 10:48:44 +0900

    third commit

--- InstagramTutorial/InstagramTutorial/Core/Feed/FeedView.swift
+++ InstagramTutorial/InstagramTutorial/Core/Feed/FeedView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0...10, id: \.self) { post in
-                    FeedCell()
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram_logo")
+                        .resizable()
+                        .frame(width: 100, height: 55)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
                 }
             }
         }
