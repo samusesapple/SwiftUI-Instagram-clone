9b8974864d1e3616ee2b09552aa886c0f50bf6a8
samusesapple <gwansammy@gmail.com>
2023-07-07 18:25:42 +0900

    configure Tutorial-1

---
+++ SwiftUI-Tutorial-1/SwiftUI-Tutorial-1/SwiftUI_Tutorial_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_1App.swift
+//  SwiftUI-Tutorial-1
+//
+//  Created by Sam Sung on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
