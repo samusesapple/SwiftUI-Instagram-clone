92dad6368fe42ab379d8dc3cf0aaca4c2599421a
samusesapple <gwansammy@gmail.com>
2023-07-15 17:26:52 +0900

    set 2nd tutorial file

---
+++ SwiftUI-Tutorial-2/SwiftUI-Tutorial-2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Tutorial-2
+//
+//  Created by Sam Sung on 2023/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Tutorial-2/SwiftUI-Tutorial-2/SwiftUI_Tutorial_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_2App.swift
+//  SwiftUI-Tutorial-2
+//
+//  Created by Sam Sung on 2023/07/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
