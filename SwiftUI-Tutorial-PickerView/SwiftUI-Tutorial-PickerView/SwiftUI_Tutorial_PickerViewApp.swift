73d4c76867c50d89e347764981a35e32461df248
Sam Sung <126672733+samusesapple@users.noreply.github.com>
2023-10-04 18:48:12 +0900

    Update SwiftUI_Tutorial_PickerViewApp.swift

--- SwiftUI-Tutorial-PickerView/SwiftUI-Tutorial-PickerView/SwiftUI_Tutorial_PickerViewApp.swift
+++ SwiftUI-Tutorial-PickerView/SwiftUI-Tutorial-PickerView/SwiftUI_Tutorial_PickerViewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Tutorial_PickerViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FriendList()
         }
     }
 }
